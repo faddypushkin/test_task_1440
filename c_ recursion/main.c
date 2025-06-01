0629e4107702d0b161d84dc9af3e195366d9055b
dstepanenko <dstepanenko@aq.ru>
2025-06-01 18:46:58 +0300

    Добавление решения задачи про рекурсию

--- c_ recursion/main.c
+++ c_ recursion/main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 
+int f(int n);
+
 int main(void)
 {
-	printf("Hello world!\n");
+	int num;
+	int step = 0;
+	int a[10] = {0};
 
+	for (int i = 0; i < 14; i++) {
+		num = f(i);
+
+		if (num % 2 == 1) {
+			a[step] = num;
+			step++;
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+		printf("A[%d] = %d\n", i, a[i]);
+	
 	return 0;
+}
+
+int f(int n)
+{
+	if (n == 0)
+		return 1;
+
+	if (n == 1)
+		return 3;
+
+	return 5 * f(n - 1) + f(n - 2);
 }
